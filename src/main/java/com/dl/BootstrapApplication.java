caf5387a66ce115f602a49065ac903d2d1f41a3b
lhq <luohuaqiang_cd@keruyun.com>
2018-09-17 11:32:12 +0800

    搭建项目框架

---
+++ src/main/java/com/dl/BootstrapApplication.java
@@ -0,0 +1,19 @@
+package com.dl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author Li Lun
+ * @date 2018/9/17 11:09
+ * @description this is description
+ */
+
+@SpringBootApplication(scanBasePackages = "com.dl", exclude = DataSourceAutoConfiguration.class)
+public class BootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapApplication.class, args);
+    }
+}
